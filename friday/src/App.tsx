b5de8cac5fefd9058c350b74c9a9bd8b8cd01a1f
victorrmugi <victor.mugi@strathmore.edu>
2026-06-27 13:23:03 +0300

    nav bar

--- friday/src/App.tsx
+++
@@ -1,9 +0,0 @@
-import Greeting from "./Components/Content";
-function App() {
-  return (
-    <> 
-      <Greeting />
-    </>
-  );
-}
-export default App;
--- friday/src/Components/Content.tsx
+++
@@ -1,5 +0,0 @@
-function Greeting() {
-    const fname = "John";
-    return <h1>Good Morning, {fname}!</h1>;
-}
-export default Greeting;
